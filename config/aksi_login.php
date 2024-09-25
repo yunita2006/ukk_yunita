c0a44ef13dfb00f9629ae249073ac6815fe52679
yunita2006 <ynita4527@gmail.com>
2024-09-25 09:09:04 +0800

    Add files via upload

---
+++ config/aksi_login.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include 'koneksi.php';
+
+$username = $_POST['username'];
+$password = md5($_POST['password']);
+
+$sql = mysqli_query($koneksi, "SELECT * FROM user WHERE username='$username' AND password='$password'");
+
+$cek = mysqli_num_rows($sql);
+
+if ($cek > 0) {
+    $data = mysqli_fetch_array($sql);
+
+    $_SESSION['username'] = $data['username'];
+    $_SESSION['userid'] = $data['userid'];
+    $_SESSION['status'] = 'login';
+    echo "<script>
+    alert('Login Berhasil');
+    location.href='../admin/index.php';
+    </script>";
+}else{
+    echo "<script>
+    alert('Username atau Password Salah!');
+    location.href='../login.php';
+    </script>";
+}
+
+?>
---
+++ config/aksi_register.php
@@ -0,0 +1,19 @@
+<?php  
+include "koneksi.php";
+
+$username = $_POST['username'];
+$password = md5($_POST['password']);
+$email = $_POST['email'];
+$namalengkap = $_POST['namalengkap'];
+$alamat = $_POST['alamat'];
+
+$sql = mysqli_query($koneksi, "INSERT INTO user VALUES ('','$username','$password','$email','$namalengkap','$alamat')");
+
+ if ($sql) {
+    echo "<script>
+    alert('Pendaftaran akun berhasil');
+    location.href='../login.php';
+    </script>";
+ }
+
+?>
